51258c3753432593b0cf9a3621e8d9da328f6d64
Rodrigo Bertho <rodrigobertho@gmail.com>
2023-11-13 15:00:34 -0300

    adicionando o id_device no retorno do login

--- src/models/loginModel.js
+++ src/models/loginModel.js
@@ -6,8 +6,11 @@ const login = async (req, res) => {
         req.body.password
     ]
     console.log('chamou o LOGIN: ', values[0], values[1])
-    const result = await pool.query(`SELECT * FROM users WHERE email = $1 AND password = $2`, [values[0], values[1]]);
-//    const result = await pool.query(`SELECT * FROM users WHERE email = 'rodrigobert@gmail.com' AND password = 'pass123'`);
+    let query = `SELECT U.id_user, name, email, rent_unit, id_device ` +
+                `FROM users U, device D ` +
+                `WHERE U.id_user = D.id_user ` +
+                `AND email = $1 AND password = $2`
+    const result = await pool.query(query, [values[0], values[1]]);
     return result
 };
 
