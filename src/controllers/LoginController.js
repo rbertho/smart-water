4a9c7ecf0f2f0cd91c2ae1e69927deb3bd0e59ef
Rodrigo Bertho <rodrigobertho@gmail.com>
2024-08-30 17:39:18 -0300

    Update LoginController.js

--- src/controllers/LoginController.js
+++ src/controllers/LoginController.js
@@ -3,7 +3,7 @@ const loginModel = require('../models/loginModel');
 const login = async (req, res) => {
     const result = await loginModel.login(req, res);
 
-    console.log('Registros encontrados: ', result.rows.length)
+    console.log('Registros encontrados no login: ', result.rows.length)
     let success = result.rows.length > 0;
     let returnCode = success ? 200 : 404;
     return res.status(returnCode).set({'Access-Control-Allow-Origin': '*'}).json(result.rows);
